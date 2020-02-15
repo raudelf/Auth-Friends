47d971f3cd632cc2fff2601ce84bc36a8cf9f9c3
Raudel Flores <rawdell@outlook.com>
2020-02-14 19:53:09 -0600

    reformatted

--- friends/src/components/Homepage.js
+++ friends/src/components/Homepage.js
@@ -4,9 +4,9 @@ const Homepage = () => {
     return (
     <div>
         <header className="App-header">
-        <h1>Auth Friends</h1>
-        <h2>HomePage</h2>
-    </header>
+            <h1>Auth Friends</h1>
+            <h2>HomePage</h2>
+        </header>
     </div>
     )
 }
