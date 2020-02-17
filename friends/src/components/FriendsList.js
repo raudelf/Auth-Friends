95d89a666b420a2ad2d53c604fd80634a04ebfbf
Raudel Flores <rawdell@outlook.com>
2020-02-16 23:53:44 -0600

    created the FriendsList component that displays friend info

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const FriendsList = props => {
+
+    const [friendsList, setFriendsList] = useState([]);
+    
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/friends')
+            .then(res => {
+                console.log(res);
+                setFriendsList(res.data)
+            })
+            .catch(err => {
+                console.log('Error Fetching Friends: ', err);
+            });
+    }, [])
+
+
+    return (
+        <div className='friendsContainer'>
+            {friendsList.map((friend, index) => {
+                return (
+                    <div key={index} className='friendBox'>
+                        <h3>{friend.name}</h3>
+                        <p>Age: {friend.age}</p>
+                        <p>Email: <a href={`mailto:${friend.email}`}>{friend.email}</a></p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default FriendsList;
